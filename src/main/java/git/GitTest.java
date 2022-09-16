42254f06ab2d1d218b53f9c3b2bf2a9e96a65029
xwj <786728790@qq.com>
2022-09-16 18:01:00 +0800

    master  commit

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!");
         System.out.println("hello git2!!");
-        System.out.println("hello hot-fix!!");
+        System.out.println("hello master !!");
+
     }
 }
